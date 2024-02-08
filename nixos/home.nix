89f52dee4401d6057f34c9f6b5832810a08bf1c0
G. Arends <gd@wmc.io>
2024-02-08 16:23:29 -0400

    Fix home.nix

--- nixos/home.nix
+++ nixos/home.nix
@@ -12,35 +12,35 @@
     neofetch
     fzf # A command-line fuzzy finder
 
-    # basic configuration of git, please change to your own
-    programs.git = {
-      enable = true;
-      userName = "GD";
-      userEmail = "gd@wmc.io";
-    };
-
-    # alacritty - a cross-platform, GPU-accelerated terminal emulator
-    programs.alacritty = {
-      enable = true;
-      # custom settings
-      settings = {
-        env.TERM = "xterm-256color";
-        font = {
-          size = 12;
-          draw_bold_text_with_bright_colors = true;
-        };
-        scrolling.multiplier = 5;
-        selection.save_to_clipboard = true;
-      };
-    };
-
-    # You can update home Manager without changing this value. See
-    # the home Manager release notes for a list of state version
-    # changes in each release.
-    home.stateVersion = "23.11";
-
-    # Let home Manager install and manage itself.
-    programs.home-manager.enable = true;
-
   ];
+
+  # basic configuration of git, please change to your own
+  programs.git = {
+    enable = true;
+    userName = "GD";
+    userEmail = "gd@wmc.io";
+  };
+
+  # alacritty - a cross-platform, GPU-accelerated terminal emulator
+  programs.alacritty = {
+    enable = true;
+    # custom settings
+    settings = {
+      env.TERM = "xterm-256color";
+      font = {
+        size = 12;
+        draw_bold_text_with_bright_colors = true;
+      };
+      scrolling.multiplier = 5;
+      selection.save_to_clipboard = true;
+    };
+  };
+
+  # You can update home Manager without changing this value. See
+  # the home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "23.11";
+
+  # Let home Manager install and manage itself.
+  programs.home-manager.enable = true;
 }
