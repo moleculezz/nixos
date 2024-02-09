e1267af7f47e4c5eac714d70f3a69c26b68f2a2b
GD <gd@wmc.io>
2024-02-08 22:48:10 -0400

    Remove updateMicrocode, it's already being called by nixos-hardware

--- nixos/hardware-configuration.nix
+++ nixos/hardware-configuration.nix
@@ -51,5 +51,5 @@
   # networking.interfaces.wlp1s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  # hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
