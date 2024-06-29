642d21bd5a1170f4ddef915afee91d237842d147
GD <gd@wmc.io>
2024-06-29 08:52:39 -0400

    Add bluetooth

--- hosts/gnix/configuration.nix
+++ hosts/gnix/configuration.nix
@@ -72,6 +72,15 @@
     alsa.support32Bit = true;
     pulse.enable = true;
   };
+
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+    # package = pkgs.bluez5-experimental;
+    settings.Policy.AutoEnable = "true";
+    settings.General.Enable = "Source,Sink,Media,Socket";
+  };
+  services.blueman.enable = true;
   
   services.fwupd.enable = true;
 
