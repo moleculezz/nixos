eb492c7c593d9fd51e1d90bb7267fdaa8f08d0a2
GD <gd@wmc.io>
2024-09-14 08:21:29 -0400

    Don't install everything for now.

--- nixosModules/default.nix
+++ nixosModules/default.nix
@@ -7,10 +7,10 @@
       #./hyprland
       #./fonts
       ./utilities.nix
-      ./office.nix
-      ./multimedia.nix
-      ./development.nix
-      ./crypto.nix
-      ./libvirt.nix
+      #./office.nix
+      #./multimedia.nix
+      #./development.nix
+      #./crypto.nix
+      #./libvirt.nix
     ];
   }
