3fe687ce25bd9befd90c9e5afc3fa61d1ae7b280
GD <gd@wmc.io>
2024-09-14 12:50:58 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,9 +2,9 @@
 
 cd /tmp
 nix-shell -p git
-git checkout kde-luks
 git clone https://github.com/moleculezz/nixos-config
-exit
 cd nixos-config
+git checkout kde-luks
+exit
 sudo nix run 'github:nix-community/disko#disko-install' --experimental-features 'nix-command flakes' -- --write-efi-boot-entries --flake '/tmp/nixos-config#gnix' --disk nvme /dev/nvme0n1
 
