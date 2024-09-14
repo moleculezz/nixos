45fc840a8129f811d839c37e7ef5215a87d2ac10
GD <gd@wmc.io>
2024-09-14 12:14:50 -0400

    fix the setup script

--- setup.sh
+++ setup.sh
@@ -3,8 +3,8 @@
 cd /tmp
 nix-shell -p git
 git checkout kde-luks
-git clone https://github.com/moleculezz/nixos-config .nixos-config
+git clone https://github.com/moleculezz/nixos-config
 exit
-cd .nixos-config
-sudo nix run 'github:nix-community/disko#disko-install' --experimental-features 'nix-command flakes' -- --write-efi-boot-entries --flake '/tmp/nixos#gnix' --disk nvme /dev/nvme0n1
+cd nixos-config
+sudo nix run 'github:nix-community/disko#disko-install' --experimental-features 'nix-command flakes' -- --write-efi-boot-entries --flake '/tmp/nixos-config#gnix' --disk nvme /dev/nvme0n1
 
