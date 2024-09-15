ea5f86f869aae5fdd62645b0d74b491d0fea8266
GD <gd@wmc.io>
2024-09-15 12:24:34 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,10 +4,10 @@ echo -n "Enter crypto password: "
 read password
 
 cd /tmp
-nix run noxpkgs#git --experimental-features 'nix-command flakes' -- clone https://github.com/moleculezz/nixos-config
+nix run nixpkgs#git --experimental-features 'nix-command flakes' -- clone https://github.com/moleculezz/nixos-config
 cd nixos-config
-nix run noxpkgs#git --experimental-features 'nix-command flakes' -- checkout kde-luks
-echo -n "$passeord" > /tmp/secret.key
+nix run nixpkgs#git --experimental-features 'nix-command flakes' -- checkout kde-luks
+echo -n "$password" > /tmp/secret.key
 #sudo nix run 'github:nix-community/disko#disko-install' --experimental-features 'nix-command flakes' -- --write-efi-boot-entries --flake '/tmp/nixos-config#gnix' --disk nvme /dev/nvme0n1
 #sudo systemd-cryptenroll --fido2-device=auto /dev/nvme0n1p3
 
