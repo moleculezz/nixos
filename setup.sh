2f0d351bffb58571e6f8dcaf2a521103e7a2fb01
GD <gd@wmc.io>
2024-09-15 12:15:28 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,13 @@
 #!/run/current-system/sw/bin/bash
 
-#cd /tmp
-#nix-shell -p git
-#git clone https://github.com/moleculezz/nixos-config
-#cd nixos-config
-#git checkout kde-luks
-#exit
-#echo -n "Password" > /tmp/secret.key
-sudo nix run 'github:nix-community/disko#disko-install' --experimental-features 'nix-command flakes' -- --write-efi-boot-entries --flake '/tmp/nixos-config#gnix' --disk nvme /dev/nvme0n1
-sudo systemd-cryptenroll --fido2-device=auto --fido2-with-user-verification=true /dev/nvme0n1p3
+echo -n "Enter crypto password: "
+read $password
+
+cd /tmp
+nix run noxpkgs#git --experimental-features 'nix-command flakes' -- clone https://github.com/moleculezz/nixos-config
+cd nixos-config
+nix run noxpkgs#git --experimental-features 'nix-command flakes' -- checkout kde-luks
+echo -n "$passeord" > /tmp/secret.key
+#sudo nix run 'github:nix-community/disko#disko-install' --experimental-features 'nix-command flakes' -- --write-efi-boot-entries --flake '/tmp/nixos-config#gnix' --disk nvme /dev/nvme0n1
+#sudo systemd-cryptenroll --fido2-device=auto /dev/nvme0n1p3
 
