5f8cf37f1a98955622299c8f94d092182bf6190e
GD <gd@wmc.io>
2024-09-24 12:48:46 -0400

    Add signal and obsidian

--- nixosModules/office.nix
+++ nixosModules/office.nix
@@ -4,6 +4,8 @@
     libreoffice-qt-fresh # QT version for KDE
     pdfmixtool # OpenSource pdf merge/split tool
     xournalpp # OpenSource pdf editor, annotate
+    signal-desktop
+    obsidian
     fork.pdfstudio2024 # NonFree pdf editor, similar to Acrobat
   ];
 }
