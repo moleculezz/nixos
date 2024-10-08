d87d20c4ac9a33ecca65b36cad4eeb89042e6023
GD <gd@wmc.io>
2024-10-08 07:55:16 -0400

    Add trezor wallet

--- nixosModules/crypto.nix
+++ nixosModules/crypto.nix
@@ -3,5 +3,8 @@
   environment.systemPackages = with pkgs; [
     cryptsetup #LUKS setup
     yubikey-manager #FIDO2 setup
+    trezor-suite #Crypto Wallet
+    trezor-udev-rules
+    trezorctl
   ];
 }
